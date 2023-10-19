ca0f8e17aa38d9b9275aa87eb9e4cc79dd713ac6
Spark4545 <lerouxpaulhenri@gmail.com>
2023-10-19 21:37:43 +0200

    Navigation, frontend and style bases

---
+++ frontend/src/styles/inspectorStyle.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from "react-native";
+
+const inspectorStyle = StyleSheet.create({
+  title: {
+    color: "black",
+    fontSize: 40,
+    fontWeight: "bold",
+    marginTop: 30,
+    marginBottom: 20,
+  },
+  text: {
+    color: "black",
+    fontSize: 20,
+  },
+  
+});
+
+export default inspectorStyle;
