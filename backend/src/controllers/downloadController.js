0323822437192ca7533b660c06cc24b4cb1193da
nimrod02 <letrun.a@gmail.com>
2023-10-25 11:47:19 +0200

    add index.js and src/controllers

---
+++ backend/src/controllers/downloadController.js
@@ -0,0 +1,22 @@
+const path = require('path');
+const fs = require('fs');
+
+exports.downloadFile = (req, res) => {
+  const fileName = '100MB.dat';
+  const filePath = path.join(__dirname, 'utils', fileName);
+
+  console.log('Requested file path:', filePath); 
+
+  if (fs.existsSync(filePath)) {
+    console.log('File exists.'); 
+    res.download(filePath, fileName, (err) => {
+      if (err) {
+        console.error('Error downloading file:', err); 
+        res.status(500).send('Error downloading file');
+      }
+    });
+  } else {
+    console.log('File does not exist.'); 
+    res.status(404).send('The file requested does not exist');
+  }
+};
