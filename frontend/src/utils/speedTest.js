a1a8fc694175f873cc47b5f28bc9893c9237dcb1
nimrod02 <letrun.a@gmail.com>
2023-10-26 10:21:51 +0200

    add speedtest feature with working download

--- frontend/src/utils/speedTest.js
+++ frontend/src/utils/speedTest.js
@@ -1,71 +1,42 @@
-import React, { useState } from 'react';
-import { View, Text, Button, ActivityIndicator, StyleSheet } from 'react-native';
 import axios from 'axios';
 
-const SpeedTest = () => {
-  const [downloadSpeed, setDownloadSpeed] = useState(null);
-  const [uploadSpeed, setUploadSpeed] = useState(null);
-  const [isLoading, setIsLoading] = useState(false);
-
-  const runSpeedTest = async () => {
-    setIsLoading(true);
-  
-    try {
-      // Download speed test
-      const startDownloadTime = Date.now();
-      console.log('working1');
-      await axios.get('http://192.168.1.120:3000/download');
-      console.log('working2');
-      const endDownloadTime = Date.now();
-  
-      const downloadTime = endDownloadTime - startDownloadTime;
-      const downloadSpeedMbps = (100 / (downloadTime / 1000) * 8); // Mbps
-      console.log('Download speed working');
-      
-      // Upload speed test
-      const startUploadTime = Date.now();
-      const response = await axios.post('http://192.168.1.120:3000/upload', {
-        file: '100MB.dat',
-      });
-      console.log('Upload speed working');
-  
-      const endUploadTime = Date.now();
-      const uploadTime = endUploadTime - startUploadTime;
-      const uploadSpeedMbps = (100 / (uploadTime / 1000) * 8); // Mbps
-  
-      setDownloadSpeed(downloadSpeedMbps);
-      setUploadSpeed(uploadSpeedMbps);
-    } catch (error) {
-      console.error('Speed test failed: ', error);
-    }
-  
-    setIsLoading(false);
-  };  
-
-  return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Speed Test</Text>
-      {isLoading ? (
-        <ActivityIndicator size="large" color="#0000ff" />
-      ) : (
-        <Button title="Run Speed Test" onPress={runSpeedTest} />
-      )}
-      {downloadSpeed && <Text style={styles.text}>Download Speed: {downloadSpeed.toFixed(2)} Mbps</Text>}
-      {uploadSpeed && <Text style={styles.text}>Upload Speed: {uploadSpeed.toFixed(2)} Mbps</Text>}
-    </View>
-  );
+const downSpeedTest = async () => {
+  // downloadTest
+  const startDownloadTime = Date.now();
+  console.log('start download...');
+  const res = await axios.get('http://192.168.1.120:3000/download');
+  console.log('end download.');
+  const endDownloadTime = Date.now();
+  const downloadTime = endDownloadTime - startDownloadTime;
+  const downloadSpeed = (100 / downloadTime * 1000) * 8;
+  return downloadSpeed;
 };
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  text: {
-    fontSize: 20,
-    marginBottom: 10,
-  },
-});
+// // uploadTest
+// const upSpeedTest = async (file) => {
+//   const startUploadTime = Date.now();
+//   console.log('start upload...');
 
-export default SpeedTest;
+//   const formData = new FormData();
+//   formData.append('40MB.dat', file);
+
+//   try {
+//     const response = await axios.post('http://192.168.1.120:3000/upload', formData, {
+//       headers: {
+//         'Content-Type': 'multipart/form-data',
+//       },
+//     });
+
+//     console.log('end upload.');
+//     const endUploadTime = Date.now();
+//     const uploadTime = endUploadTime - startUploadTime;
+//     const uploadSpeed = (file.size / uploadTime * 1000) * 8; // Mbps
+
+//     return uploadSpeed;
+//   } catch (error) {
+//     console.error('Upload failed:', error);
+//     return 0;
+//   }
+// };
+
+export { downSpeedTest };
